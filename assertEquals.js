199b05a1eb553addba7b28304c54a3f2569028f9
Geneen <geneen850@gmail.com>
2025-07-20 15:19:38 +0300

    convert ES5 to ES6

---
+++ assertEquals.js
@@ -0,0 +1,43 @@
+// convert to arrow function
+const sumNum = (num1, num2) => {
+  return num1 + num2;
+};
+
+const odds = evens.map(function (v) {
+  return v + 1;
+});
+
+// Add default parameters to sum to have the same functionality
+const sum = (x = 0, y = 7, z = 42) => {
+  return x + y + z;
+};
+
+const calculateArea = (height, width) => {
+  height = height || 50;
+  width = width || 80;
+  return height * width;
+};
+
+// destruct function arguments
+const getUserData = ({ name: { firstName, lastName }, age }) => {
+  console.log(firstName, lastName, age);
+};
+
+// what about array destructuring
+let list = [1, 2, 3];
+let [a, , b] = list;
+
+
+// use template literals
+let customer = { name: 'Foo' };
+let card = { amount: 7, product: 'Bar', unitprice: 42 };
+
+
+let message = `Hello ${customer.name},
+want to buy ${card.amount} ${card.product} for
+ total of ${card.amount * card.unitprice} bucks?`;
+
+// short syntax for object properties in obj below
+let x = 0;
+let y = 0;
+let obj = { x, y };
